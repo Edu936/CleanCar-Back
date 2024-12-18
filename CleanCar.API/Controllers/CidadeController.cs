64aad068e25f2c10a01384ede80c3bd6d5dd1ece
Eduardo Vieira Marques <eduvieira936@gmail.com>
2024-12-18 11:40:17 -0300

    feat: created CidadeController

---
+++ CleanCar.API/Controllers/CidadeController.cs
@@ -0,0 +1,18 @@
+﻿using CleanCar.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanCar.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CidadeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Olá mundo";
+        }
+
+    }
+}
---
+++ CleanCar.Domain/Models/Cidade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCar.Domain.Models
+{
+    public class Cidade
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Estado { get; set; }
+    }
+}
